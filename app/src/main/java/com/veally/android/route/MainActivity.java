0624c686d58f88fc3c01f87460a9cd4e7d2c5886
wei.tang5 <wei.tang5@56qq.com>
2018-06-11 17:52:17 +0800

    modify dependencies

--- app/src/main/java/com/veally/android/route/MainActivity.java
+++ app/src/main/java/com/veally/android/route/MainActivity.java
@@ -2,6 +2,10 @@ package com.veally.android.route;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.veally.android.ipay.IPayType;
+import com.veally.android.paybridge.PayBridge;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cashpay(View v){
+        new PayBridge.Builder().setPayType(IPayType.CASHPAY).build().pay();
+    }
+
+    public void alipay(View view) {
+        new PayBridge.Builder().setPayType(IPayType.ALIPAY).build().pay();
     }
 }
